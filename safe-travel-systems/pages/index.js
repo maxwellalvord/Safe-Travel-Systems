2fb0817171049339a5fcbfdb9d3a3e7c10fc5c8c
Maxwell Alvord <maxwellalvord@gmail.com>
2022-09-16 11:18:20 -0700

    update research and trouble shoot using next/link

---
+++ safe-travel-systems/pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+    <Link href='/about'><a>About</a></Link>
+    </>
+  )
+}
