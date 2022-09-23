5280945e507ca6a7f08c174fed08f84165870f75
Maxwell Alvord <maxwellalvord@gmail.com>
2022-09-23 16:10:59 -0700

    WIP on currentlyVisable conditional

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import styles from '../styles/Home.module.css'
 import Header from './header';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
 
 const Head = styled.a`
 color: #5c8c9c;
@@ -8,12 +9,27 @@ color: #5c8c9c;
 
 export default function Home() {
   
+  const [formVisableOnPage, setFormVisableOnPage] = useState(false); 
+  
   let currentlyVisibleState = null;
 
-  if (selectedDestination != null) {
-    currentlyVisibleState = <DestinationDetail
-    
+  if (editing){
+    currentlyVisibleState = <EditDestinationForm
+    destination = {selectedDestination}
+    onEditDestination ={handleEditingDestinationList}
+    />
   }
+  else if (selectedDestination != null) {
+    currentlyVisibleState = <DestinationDetail
+    destination = {selectedDestination}
+    onClickingDelete = {handleDeletingDestination}
+    OnClickingEdit = {handleEditingDestination}
+    />
+  } else if (formVisableOnPage) {
+    currentlyVisibleState = <newDestinationForm
+    onNewDestinationCreation = {handleChangingSelectedDestination}
+  }
+
 
   return (
     <>
