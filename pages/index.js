f392e7415b2aae29bba1fac7f1307b460633a6d7
Maxwell Alvord <maxwellalvord@gmail.com>
2022-09-23 11:34:21 -0700

    add css librarys and experiment

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+// import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styled from 'styled-components';
+
+
 
 export default function Home() {
+
+  const A = styled.a`
+    color: #5c8c9c;
+    hover: cursor;
+  `;
+
   return (
     <>
-    <Link href='/about'><a>About</a></Link>
+      <Link href='/about'><A >About</A></Link> &emsp;
+      <Link href='/'><A>Home</A></Link> &emsp;
+      <Link href='/signin'><A>Sign In</A></Link>
     </>
   )
 }
