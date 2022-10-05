3b82a944149bc9c304a49a7b5c99be77ddaa467d
Maxwell Alvord <maxwellalvord@gmail.com>
2022-10-05 11:23:38 -0700

    add text to about page

--- pages/footer.js
+++ pages/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <div className='footer'>
-        <p><a href = "mailto:maxwellalvord@gmail.com">maxwellalvord@gmail.com</a> ● 503-830-9753 ● <a href = "https://github.com/maxwellalvord">Github: maxwellalvord</a> ● <a href = "https://www.linkedin.com/in/maxwellalvord/">LinkedIn: maxwellalvord</a></p>
+        <p><a href = "mailto:maxwellalvord@gmail.com">maxwellalvord@gmail.com</a> ● <a href = "https://github.com/maxwellalvord">Github: maxwellalvord</a> ● <a href = "https://www.linkedin.com/in/maxwellalvord/">LinkedIn: maxwellalvord</a></p>
         <p>Copyright © 2022 Maxxity Inc. All rights reserved.</p>
       </div>
     </>
