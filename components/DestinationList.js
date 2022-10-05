de4a7914ec5d1e2ab42c39c1c0fce00bfcb1494b
Maxwell Alvord <maxwellalvord@gmail.com>
2022-10-05 13:41:22 -0700

    reverting change that broke live site

--- components/DestinationList.js
+++ components/DestinationList.js
@@ -4,10 +4,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Peralta&display=swap');
-</style>
-
 const Dest = styled.ul`
   background-color: #0EA268;
   width: 1230px;
@@ -16,7 +12,6 @@ const Dest = styled.ul`
   border: 2px solid #FFFBC8;
   padding: 25px;
   border-radius: 10px;
-  font-family: 'Peralta', cursive;
 `;
 
 export default function DestinationList(props){
